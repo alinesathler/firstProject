05d88663afd8f24e053844e4b647c56062884451
Aline Sathler <sathler@ymail.com>
2023-02-25 11:24:32 -0300

    Initial version

---
+++ README.txt
@@ -0,0 +1,1 @@
+First project: training HTML, CSS and Jaavascript - basic
---
+++ scripts.js
@@ -0,0 +1,29 @@
+function calculateTip(event) {
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let ServiceQuality = document.getElementById('ServiceQuality').value;
+    let people = document.getElementById('people').value;
+
+    if(bill==''|ServiceQuality==0){
+        alert("Please, full fill the form")
+        return;
+    }
+
+    if(people==''|people<=1){
+        people=1;
+        document.getElementById('each').style.display="none"
+    } else{
+        document.getElementById('each').style.display="block"
+    }
+
+    let total=(bill*ServiceQuality)/people;
+    total=total.toFixed(2);
+    document.getElementById('tip').innerHTML=total;
+    document.getElementById('totalTip').style.display="block";
+
+}
+
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip)
